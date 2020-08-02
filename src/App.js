76af0ad3301b8a2379aaa23eaa64491e894ecfd5
Stefan Smeu <smeustefan090@gmail.com>
2020-08-02 14:03:08 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ function App() {
       <Button variant="contained" color="primary">
         Hello World
       </Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
